d9df5cf09a13660c22d26914b77a50884e9d3da7
Bektur <project.bekturtilepbek@gmail.com>
2024-06-26 17:13:37 +0600

    вывел на главную страницу список задач, добавил страницу добавления задачи

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.views import index
+from webapp.views import index, new_task
 
 urlpatterns = [
     path('', index),
+    path('task/new/', new_task)
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,21 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from webapp.models import Task, status_choices
 
 
 def index(request):
-    return render(request, 'index.html')
+    tasks = Task.objects.all()
+    return render(request, 'index.html', context={'tasks': tasks, 'choices': status_choices})
+
+
+def new_task(request):
+    if request.method == 'GET':
+        return render(request, 'new_task.html', context={'choices': status_choices})
+    else:
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        complete_date = request.POST.get('complete_date')
+        print(description, status, complete_date)
+        Task.objects.create(description=description, status=status, complete_date=complete_date)
+        return HttpResponseRedirect('/')
