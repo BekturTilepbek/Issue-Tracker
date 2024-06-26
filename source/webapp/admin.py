9731c26ff213c5c48a9ed09bd591317935dd85a9
Bektur <project.bekturtilepbek@gmail.com>
2024-06-26 15:53:51 +0600

    создал модель, зарегистрировал модель, провел миграции

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'complete_date']
+    list_display_links = ['id', 'description']
+    list_filter = ['status']
+    search_fields = ['description', 'status']
+    fields = ['description', 'status', 'complete_date']
+
+
+admin.site.register(Task, TaskAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+
+
+class Task(models.Model):
+    description = models.CharField(max_length=200, null=False, blank=False, verbose_name='Описание')
+    status = models.CharField(max_length=50, null=False, blank=False, default="new", choices=status_choices, verbose_name='Статус')
+    complete_date = models.DateField(verbose_name="Дата выполнения")
+
+    def __str__(self):
+        return f"{self.pk}. {self.description}: {self.status}"
+
+    class Meta:
+        db_table = "tasks"
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
