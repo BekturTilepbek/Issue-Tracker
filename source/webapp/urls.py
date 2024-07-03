002181c86017efe3a4b5a86594c55b1e4313cddc
Bektur <project.bekturtilepbek@gmail.com>
2024-07-04 04:38:16 +0600

    добавил изменение и удаление задач, обновил шаблоны
    обновил фикстуры

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from webapp.views import index, create_task, read_task
+from webapp.views import index, create_task, read_task, update_task, delete_task
 
 urlpatterns = [
     path('', index, name='tasks'),
     path('task/new/', create_task, name='new_task'),
-    path('task/<int:pk>/', read_task, name='detail_task')
+    path('task/<int:pk>/', read_task, name='detail_task'),
+    path('task/<int:pk>/update/', update_task, name='update_task'),
+    path('task/<int:pk>/delete/', delete_task, name='delete_task'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -16,12 +16,8 @@ def create_task(request):
     else:
         form = TaskForm(data=request.POST)
         if form.is_valid():
-            task = Task.objects.create(
-                description=request.POST.get('description'),
-                detailed_description=request.POST.get('detailed_description') if request.POST.get('detailed_description') else "",
-                status=request.POST.get('status'),
-                complete_date=request.POST.get('complete_date') if request.POST.get('complete_date') else None)
-            return redirect("tasks")
+            task = form.save()
+            return redirect("detail_task", pk=task.pk)
 
         return render(request, 'new_task.html', context={'form': form})
 
@@ -29,3 +25,31 @@ def create_task(request):
 def read_task(request, pk):
     task = get_object_or_404(Task, pk=pk)
     return render(request, 'detail_task.html', context={'task': task, 'choices': status_choices})
+
+
+def update_task(request, pk):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=pk)
+        form = TaskForm(instance=task)
+        return render(request, 'update_task.html', context={'form': form})
+    else:
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task = get_object_or_404(Task, pk=pk)
+            task.description = form.cleaned_data['description']
+            task.detailed_description = form.cleaned_data['detailed_description']
+            task.status = form.cleaned_data['status']
+            task.complete_date = form.cleaned_data['complete_date']
+            task.save()
+            return redirect("detail_task", pk=task.pk)
+        else:
+            return render(request, 'update_task.html', context={'form': form})
+
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'delete_task.html', context={'task': task})
+    else:
+        task.delete()
+        return redirect("tasks")
