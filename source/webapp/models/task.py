ee557f13c6b23eb558f7ff8586a21dc10a006e5c
Bektur <project.bekturtilepbek@gmail.com>
2024-07-21 22:21:39 +0600

    добавил задачам поле 'Проект', через дата-миграцию заполнил всем задачам поле проекта

--- source/webapp/models/task.py
+++ source/webapp/models/task.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Task(models.Model):
-    description = models.CharField(max_length=200, null=False, blank=False, verbose_name='Описание')
+    description = models.CharField(max_length=200, verbose_name='Описание')
     detailed_description = models.TextField(null=True, blank=True, default="", verbose_name='Подробное описание')
     status = models.ForeignKey('webapp.Status', on_delete=models.PROTECT, related_name='tasks', verbose_name='Статус')
     types = models.ManyToManyField('webapp.Type', related_name='tasks', verbose_name='Типы', db_table='tasks_types')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    project = models.ForeignKey('webapp.Project', on_delete=models.PROTECT, related_name='tasks', verbose_name='Проект')
 
     def __str__(self):
         return f"{self.pk}. {self.description}: {self.status}"
